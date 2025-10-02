edf89a276109e745695f5cf31c7be95956025360
Ranjini-V <ranjini.v.iyer35@gmail.com>
2025-10-03 02:23:23 +0530

    Dynamic dropdowns

---
+++ src/test/java/ranjini/v/Selenium_Practice/DynamicDropdowns.java
@@ -0,0 +1,29 @@
+package ranjini.v.Selenium_Practice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DynamicDropdowns {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		 WebDriver driver = new ChromeDriver();
+		 
+		 driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+		 driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		 driver.findElement(By.xpath("//a[@value='TIR']")).click();
+		 Thread.sleep(2000);
+		 
+		 //driver.findElement(By.xpath("(//a[@value='VTZ'])[2]")).click();
+		 // Parent-child traverse to avoid index in xpath
+		 driver.findElement(By.xpath("//div[@id='ctl00_mainContent_ddl_destinationStation1_CTNR']//a[@value='VTZ']")).click();
+		 
+		 
+		 
+		 
+		 
+
+	}
+
+}
