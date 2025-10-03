2b037cd5457141fc08ac7df483e9aa05c1d00fa2
Ranjini-V <ranjini.v.iyer35@gmail.com>
2025-10-03 16:30:43 +0530

    Checkboxes

---
+++ src/test/java/ranjini/v/Selenium_Practice/Checkboxes.java
@@ -0,0 +1,46 @@
+package ranjini.v.Selenium_Practice;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Checkboxes {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
+
+		System.out.println(
+				"Checkbox selected?:" + driver.findElement(By.id("ctl00_mainContent_SeniorCitizenDiv")).isSelected());
+		driver.findElement(By.id("ctl00_mainContent_SeniorCitizenDiv")).click();
+		System.out.println(
+				"Checkbox selected?:" + driver.findElement(By.id("ctl00_mainContent_SeniorCitizenDiv")).isSelected());
+
+		driver.findElement(By.id("ctl00_mainContent_SeniorCitizenDiv")).click();
+
+		int numberOfCheckboxes = driver.findElements(By.xpath("//input[@type='checkbox']")).size();
+		System.out
+				.println("number of checkboxes: " + numberOfCheckboxes);
+		Thread.sleep(2000);
+
+		List<WebElement> checkboxes = driver.findElements((By.xpath("//input[@type='checkbox']")));
+		for (WebElement selectCheckbox : checkboxes) {
+
+			// valid -(//input[@type='checkbox'])[1]
+
+			// since checkbox 1 in DOM is hidden - i starts from 2
+			for (int i = 2; i <= numberOfCheckboxes; i++) {
+				driver.findElement(By.xpath("(//input[@type='checkbox'])" + "[" + i + "]")).click();
+
+			}
+			break;
+		}
+		driver.close();
+	}
+
+}
