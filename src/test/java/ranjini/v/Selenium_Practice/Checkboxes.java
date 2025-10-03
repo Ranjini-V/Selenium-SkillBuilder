4a2580bcbe94fe395cc7377ba4965e1c49686086
Ranjini-V <ranjini.v.iyer35@gmail.com>
2025-10-03 16:42:54 +0530

    Checkboxes

--- src/test/java/ranjini/v/Selenium_Practice/Checkboxes.java
+++ src/test/java/ranjini/v/Selenium_Practice/Checkboxes.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 public class Checkboxes {
 
@@ -14,9 +15,8 @@ public class Checkboxes {
 		WebDriver driver = new ChromeDriver();
 
 		driver.get("https://rahulshettyacademy.com/dropdownsPractise/");
-
-		System.out.println(
-				"Checkbox selected?:" + driver.findElement(By.id("ctl00_mainContent_SeniorCitizenDiv")).isSelected());
+		Assert.assertFalse(driver.findElement(By.id("ctl00_mainContent_SeniorCitizenDiv")).isSelected());
+		Assert.assertFalse(false);
 		driver.findElement(By.id("ctl00_mainContent_SeniorCitizenDiv")).click();
 		System.out.println(
 				"Checkbox selected?:" + driver.findElement(By.id("ctl00_mainContent_SeniorCitizenDiv")).isSelected());
@@ -26,17 +26,16 @@ public class Checkboxes {
 		int numberOfCheckboxes = driver.findElements(By.xpath("//input[@type='checkbox']")).size();
 		System.out
 				.println("number of checkboxes: " + numberOfCheckboxes);
+		Assert.assertEquals(numberOfCheckboxes, 6);
 		Thread.sleep(2000);
 
 		List<WebElement> checkboxes = driver.findElements((By.xpath("//input[@type='checkbox']")));
 		for (WebElement selectCheckbox : checkboxes) {
 
 			// valid -(//input[@type='checkbox'])[1]
-
 			// since checkbox 1 in DOM is hidden - i starts from 2
 			for (int i = 2; i <= numberOfCheckboxes; i++) {
 				driver.findElement(By.xpath("(//input[@type='checkbox'])" + "[" + i + "]")).click();
-
 			}
 			break;
 		}
