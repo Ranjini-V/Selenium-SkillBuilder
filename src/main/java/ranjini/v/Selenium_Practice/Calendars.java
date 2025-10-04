7ca69de13608299af35bdc52c2fd9284a24c4edd
Ranjini-V <ranjini.v.iyer35@gmail.com>
2025-10-04 23:35:20 +0530

    CheckBox2

--- src/main/java/ranjini/v/Selenium_Practice/Calendars.java
+++ src/main/java/ranjini/v/Selenium_Practice/Calendars.java
@@ -3,6 +3,8 @@ package ranjini.v.Selenium_Practice;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+
 
 public class Calendars {
 
@@ -15,7 +17,12 @@ public class Calendars {
 		System.out.println(driver.findElement(By.id("Div1")).getAttribute("style"));
 		driver.findElement(By.id("ctl00_mainContent_rbtnl_Trip_1")).click();
 		System.out.println(driver.findElement(By.id("Div1")).getAttribute("style"));
-		//driver.findElement(By.id("Div1")).getAttribute("style").contains(""))
+		if(driver.findElement(By.id("Div1")).getAttribute("style").contains("1")){
+			System.out.println("Return Date calendar enabled");
+			Assert.assertTrue(true);
+		} else {
+			Assert.assertTrue(false);
+		}
 
 	}
 
