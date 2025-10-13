39f91e6204312cd7efdbeb02d0bbbe272a9fb7bc
Ranjini-V <ranjini.v.iyer35@gmail.com>
2025-10-13 23:51:46 +0530

    Actions and window handles

--- src/test/java/ranjini/v/Selenium_Practice/ActionPractice.java
+++ src/test/java/ranjini/v/Selenium_Practice/ActionPractice.java
@@ -3,56 +3,33 @@ package ranjini.v.Selenium_Practice;
 import java.time.Duration;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class ActionPractice {
 
 	public static void main(String[] args) {
-	
-		
+
 		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
 		Actions action = new Actions(driver);
-		
+
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-		
+
 		driver.get("https://www.amazon.com/");
 		driver.findElement(By.xpath("//button[@class='a-button-text']")).click();
-		
-		// move cursor to specific element
-		action.moveToElement(driver.findElement(By.id("nav-link-accountList"))).build().perform();
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+		// move cursor to specific element -> right-click
+		WebElement element = driver.findElement(By.id("nav-link-accountList"));
+		action.moveToElement(element).contextClick().build().perform();
+
+		// move cursor -> click -> enter in capital letters -> select text
+		WebElement searchBar = driver.findElement(By.id("twotabsearchtextbox"));
+		action.moveToElement(searchBar).click().keyDown(Keys.SHIFT).sendKeys("pillows").doubleClick().build().perform();
+
 	}
 
 }
---
+++ src/test/java/ranjini/v/Selenium_Practice/WindowHandling.java
@@ -0,0 +1,64 @@
+package ranjini.v.Selenium_Practice;
+
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WindowHandling {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		
+		driver.get("https://rahulshettyacademy.com/loginpagePractise/");
+		
+		driver.findElement(By.cssSelector(".blinkingText")).click();
+		Set<String> windows = driver.getWindowHandles();
+		Iterator<String> it = windows.iterator();
+		String parentWindow = it.next();
+		String child = it.next();
+		
+		String text = driver.findElement(By.xpath(("//p[@class='im-para red']"))).getText();
+		System.out.println(text);
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
