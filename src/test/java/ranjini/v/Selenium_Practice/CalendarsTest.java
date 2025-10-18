7c4c78bfa770140837b121ff56d0ae83189990fb
Ranjini-V <ranjini.v.iyer35@gmail.com>
2025-10-18 21:43:20 +0530

    Calendars

---
+++ src/test/java/ranjini/v/Selenium_Practice/CalendarsTest.java
@@ -0,0 +1,79 @@
+package ranjini.v.Selenium_Practice;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CalendarsTest {
+
+	public static void main(String[] args) {
+
+		String monthNumber = "12";
+		String date = "20";
+		String year = "2000";
+
+		String fullDate = monthNumber + "/" + date + "/" + year;
+		
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		
+		driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+		
+		driver.findElement(By.xpath("//button[@class='react-date-picker__calendar-button react-date-picker__button']")).click();
+		
+		
+		// month 
+		// //button[@class='react-calendar__navigation__label']
+		WebElement month = driver.findElement(By.cssSelector(".react-calendar__navigation__label"));
+		month.click();
+		month.click();
+		
+		
+		// right arrow
+		// //button[@class='react-calendar__navigation__arrow react-calendar__navigation__next-button']
+//		WebElement rightArrow = driver.findElement(By.xpath("//button[@class='react-calendar__navigation__arrow react-calendar__navigation__next-button']"));
+//		rightArrow
+		
+		// left arrow
+		// //button[@class='react-calendar__navigation__arrow react-calendar__navigation__prev-button']
+		WebElement leftArrow = driver.findElement(By.cssSelector(".react-calendar__navigation__prev-button"));
+		leftArrow.click();
+		leftArrow.click();
+		leftArrow.click();
+		
+		// year
+		// //button[@class='react-calendar__navigation__label']
+		
+		
+		// left arrow
+		// //button[@class='react-calendar__navigation__arrow react-calendar__navigation__prev-button']
+		
+		// 2000
+		// //button[text()='2000']
+		WebElement Year2000 = driver.findElement(By.xpath("//button[text()='2000']"));
+		Year2000.click();
+		
+		// December
+		// abbr[text()='December']
+		WebElement December = driver.findElement(By.cssSelector("button.react-calendar__tile.react-calendar__year-view__months__month abbr[aria-label='December 2000']"));
+		December.click();
+		
+		
+		// 20th
+		// //abbr[text()=20]
+		WebElement date20th = driver.findElement(By.cssSelector("button.react-calendar__tile.react-calendar__month-view__days__day abbr[aria-label='December 20, 2000']"));
+		date20th.click();
+		
+		
+		// get text
+		// //div[@class='react-date-picker__inputGroup']
+		WebElement expectedDate = driver.findElement(By.cssSelector(".react-date-picker__inputGroup__input.react-date-picker__inputGroup__month"));
+		System.out.println("Expected Date : "+ expectedDate.getText());
+		
+		
+	}
+
+}
