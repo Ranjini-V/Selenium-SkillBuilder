b54569d192884dca85f2c0a50aa4aa88874c4070
Ranjini-V <ranjini.v.iyer35@gmail.com>
2025-10-20 14:27:47 +0530

    Scroll drown window and DOM Object

---
+++ src/test/java/ranjini/v/Selenium_Practice/ScrollTest.java
@@ -0,0 +1,27 @@
+package ranjini.v.Selenium_Practice;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ScrollTest {
+
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		
+		// scroll window - write in browser console to find co-ordinates
+		js.executeScript("window.scrollBy(0,500)");
+		
+		// scroll through DOM object - write in browser console to find co-ordinate
+		js.executeScript("document.querySelector('.tableFixHead').scrollTop=5000");
+		
+		
+		driver.quit();
+			
+	}
+
+}
